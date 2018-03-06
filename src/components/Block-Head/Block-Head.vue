d3a1f73fddf15e4221941d95ca3b48f7401aaa4e
fwchen <crystal.tyan@gmail.com>
2018-03-06 09:45:39 +0800

    initial block head

---
+++ src/components/Block-Head/Block-Head.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card-container">
+    <img class="me-photo" alt="" src="../../assets/me.jpeg"/>
+    <div class="black-block"></div>
+    <div class="card">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class BlockHead extends Vue {
+  }
+</script>
+
+<style scoped>
+  .me-photo {
+    width: 300px;
+  }
+
+  .black-block {
+    background-color: black;
+    width: 500px;
+    height: 500px;
+  }
+</style>
