ff209f527c316a95830efc30af6b1aee08eee833
fwchen <crystal.tyan@gmail.com>
2018-03-06 22:57:44 +0800

    update

--- src/components/Block-Head/Block-Head.vue
+++ src/components/Block-Head/Block-Head.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="card-container">
+  <div class="container">
     <img class="me-photo" alt="" src="../../assets/me.jpeg"/>
     <img class="chen" src="../../assets/chen.png"/>
     <div class="black-block"></div>
     <div class="card">
+      <div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,13 +19,43 @@
 </script>
 
 <style scoped>
+  .container {
+    
+  }
+
   .me-photo {
+    position: absolute;
     width: 300px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-200px, -350px);
+    z-index: 10;
+  }
+
+  .chen {
+    width: 200px;
+    position: absolute;
+    z-index: 20;
+  }
+
+  .card {
+    height: 250px;
+    width: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    box-shadow: 0 0 3px #bebebe;
   }
 
   .black-block {
     background-color: black;
     width: 500px;
     height: 500px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-450px, -50%);
   }
 </style>
